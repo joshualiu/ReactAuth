a30f9d112d6d1fa61d81ef7fd95483eaa5b4e796
zhonghaoliu <joshualiu94@gmail.com>
2018-10-31 12:19:32 -0400

    create user records

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
-  app.get('/', (req, res, next) => {
-    res.send(['hey'])
-  });
+  app.post('/signup', Authentication.signup)
 }
