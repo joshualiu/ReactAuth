f71bda170b3ae8df99abd1cfdb25f9f0262d7ed1
zhonghaoliu <joshualiu94@gmail.com>
2018-10-31 15:12:06 -0400

    create jwt

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,9 +1,17 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
-  
+
   if (!email || !password) {
     return res.state(422).send({ error: 'You must provide email and password'});
   }
@@ -23,7 +31,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if(err) { return next(err) }
       // Respond to request indicating the user was created
-      res.json({ success: true});
+      res.json({ token: tokenForUser(user) });   
 
     });
   })
