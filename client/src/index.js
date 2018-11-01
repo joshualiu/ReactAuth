4103d39b97cecf347a20999f565abce11e803821
zhonghaoliu <joshualiu94@gmail.com>
2018-10-31 23:57:57 -0400

    create header, wiring up react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import Welcome from './components/Welcome';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <App>
+      <Route path="/" exact component={Welcome} />
+    </App>
+  </BrowserRouter>,
+  document.querySelector('#root')
+)
