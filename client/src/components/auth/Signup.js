2b7b4feff6c82bee7568e7232a7289306665120b
zhonghaoliu <joshualiu94@gmail.com>
2018-11-01 15:28:00 -0400

    dispatching actions

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -6,7 +6,9 @@ import * as actions from "../../actions";
 class Signup extends Component {
   // writing arrow function to prevent using bind(this)
   onSubmit = (formProps) => {
-    this.props.signup(formProps)
+    this.props.signup(formProps, () => {
+      this.props.history.push('/feature')
+    })
   }
   render() {
     const { handleSubmit } = this.props;
@@ -30,14 +32,20 @@ class Signup extends Component {
             autoComplete="none"
           />
         </fieldset>
+        <div>{this.props.errorMessage}</div>
         <button>Sign Up</button>
       </form>
     )
   }
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
+
 // compose prevent from writing too many parentheses
 export default compose(
-  connect(null, actions),
+  connect(mapStateToProps, actions),
   reduxForm({ form: 'signup' })
 )(Signup);
