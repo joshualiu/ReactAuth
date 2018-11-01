d06677b06f16ff156db8d2def10081d086492045
zhonghaoliu <joshualiu94@gmail.com>
2018-11-01 16:10:53 -0400

    update header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
         <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature</Link>
+        { this.renderLinks() }
       </div>
     )
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
